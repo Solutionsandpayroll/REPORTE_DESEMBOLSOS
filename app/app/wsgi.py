8958b292f7e917a512a9e94def28c6d0e961450f
Nicolas-Ballesteros07 <automatizacion@solutionsandpayroll.com>
2026-03-15 21:04:32 -0500

    fix: correcion de settings

--- app/app/wsgi.py
+++ app/app/wsgi.py
@@ -1,7 +1,18 @@
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings') # Verifica que aquí diga 'app.settings'
+# Buscamos la ruta de la primera carpeta 'APP' (donde está manage.py)
+# __file__ es .../APP/APP/wsgi.py
+# parent es .../APP/APP/
+# grandparent es .../APP/ (Esta es la que queremos en el path)
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+# Ahora que la carpeta interna APP está en el path, 
+# podemos usar 'APP.settings' directamente.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 application = get_wsgi_application()
-app = application  # <--- ESTA LÍNEA ES CLAVE PARA VERCEL
+app = application # Vercel a veces busca la variable 'app'
