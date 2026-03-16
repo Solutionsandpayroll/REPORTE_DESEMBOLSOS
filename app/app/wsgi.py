ebc89c66c45061e4f6d8d88ddbefabdf873f793e
Nicolas-Ballesteros07 <automatizacion@solutionsandpayroll.com>
2026-03-15 21:08:13 -0500

    fix: correcion de settings y de migraciones

--- app/app/wsgi.py
+++ app/app/wsgi.py
@@ -2,16 +2,13 @@ import os
 import sys
 from django.core.wsgi import get_wsgi_application
 
-# Buscamos la ruta de la primera carpeta 'APP' (donde está manage.py)
-# __file__ es .../APP/APP/wsgi.py
-# parent es .../APP/APP/
-# grandparent es .../APP/ (Esta es la que queremos en el path)
+
 path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if path not in sys.path:
     sys.path.insert(0, path)
 
 # Ahora que la carpeta interna APP está en el path, 
-# podemos usar 'APP.settings' directamente.
+# podemos usar 'app.settings' directamente.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 application = get_wsgi_application()
